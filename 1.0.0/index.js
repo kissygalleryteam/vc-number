d3226c01dfd925d4b11c850e21464e00a9cdd97e
kissyteam <tiehang.lth@taobao.com>
2015-01-23 10:04:46 +0800

    sync daily version 1.0.0

--- 1.0.0/index.js
+++ 1.0.0/index.js
@@ -75,11 +75,12 @@ var VcNumber = Base.extend({
                     else if(e.currentTarget.className.indexOf(getCls.minus)>-1){
                         inputValue -= range;
                     }
+                    self.fire('beforeChange',{input: $target, trigger: $this});
                     self._limitRange(inputValue, $target);
                     self.fire('changing',{input: $target, trigger: $this});
                 };
                 if(e.type == 'keydown' || e.type == 'mousedown'){
-                    self.fire('beforeChange',{input: $target, trigger: $this});
+                    //self.fire('beforeChange',{input: $target, trigger: $this});
                     changeValue();
                     if(timer) {clearTimeout(timer);}
                     timer = setTimeout(function(){
@@ -116,13 +117,14 @@ var VcNumber = Base.extend({
                 else if(e.keyCode === 40){
                     inputValue -= range;
                 }
+                self.fire('beforeChange',{input: $target, trigger: e.keyCode});
                 self._limitRange(inputValue, $target);
                 self.fire('changing',{input: $target, trigger: e.keyCode});
             };
 
             if(e.keyCode === 38 || e.keyCode === 40){
                 if(e.type == 'keydown'){
-                    self.fire('beforeChange',{input: $target, trigger: e.keyCode});
+                    //self.fire('beforeChange',{input: $target, trigger: e.keyCode});
                     changeValue();
                 }
                 if(e.type == 'keyup'){
